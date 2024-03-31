9aa76564ec91e07b1190663035a87e2785aa0837
Scott12565 <nkosinathinkosi153@gmail.com>
2024-04-01 00:13:11 +0200

    working on navbar...

--- src/components/Shared/Header/Navbar.jsx
+++ src/components/Shared/Header/Navbar.jsx
@@ -1,11 +1,44 @@
+import { LuMusic2, LuSearch } from "react-icons/lu";
+import logo from "../../../assets/Logo/MelodyMarkets.png"
+import { HiOutlineShoppingBag } from "react-icons/hi2";
+
 const Navbar = () => {
     return ( 
-        <header>
-            <div id="brand"></div>
-            <div className="search-song">
-                <h1>Search Songs....</h1>
+
+        // header start
+        <header className="flex justify-between w-10/12 bg-darkLavender mx-auto py-3.5 " >
+            {/* brand */}
+            <div id="brand" className="pl-2">
+                <h3 className="flex items-center">
+                    <img src={logo} alt="Logo" className="w-[150px] "/>
+                    {/* <LuMusic2 size={30} color="dodgeblue" />
+                    <span className="font-sm font-bold text-black ">MelodyMarket</span> */}
+                </h3>
             </div>
+            {/* search-bar */}
+            <div className="search-song">
+                <div className="flex items-center border border-gray-800 rounded-2xl w-60 h-9 ">
+                    <input type="text" className="h-full bg-transparent w-[200px] text-gray-800 text-lg font-lg px-3 rounded-s-2xl outline-none " placeholder="search song here..." />
+                    <span className="cursor-pointer h-full rounded-e-2xl p-1 text-gray-800 w-[50px] ">
+                        <LuSearch  size={25} color="dodgeblue"/>
+                    </span>
+                </div>
+            </div>
+            {/* navigations */}
+            <nav className="flex">
+                <div className="nav-links flex items-center px-5 text-orange-600  ">
+                    <a href="" className="SignUpSignIn">Sign Up</a>
+                    <a href="" className="SignUpSignIn ">Log In</a>
+                    <a href="" className="group">
+                        <span>
+                            <HiOutlineShoppingBag size={26} />
+                        </span>
+                    </a>
+
+                </div>
+            </nav>
         </header>
+        
      );
 }
  
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+
+    extend: {
+      colors: {
+        paleLavender: '#E0BBE4',
+        darkLavender: '#925998'
+      }
+    },
   },
   plugins: [],
 }
