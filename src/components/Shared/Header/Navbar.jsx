3c6c844fd95a94fa5f1fc3b8afdb7bbaec4cfde7
Scott12565 <nkosinathinkosi153@gmail.com>
2024-03-31 19:14:55 +0200

    First Commit

---
+++ src/components/Shared/Header/Navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return ( 
+        <header>
+            <div id="brand"></div>
+            <div className="search-song">
+                
+            </div>
+        </header>
+     );
+}
+ 
+export default Navbar;
