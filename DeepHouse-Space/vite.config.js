6f6dc77f30b99f8bbef1e0e242e09fd9daad2b49
Scott12565 <nkosinathinkosi153@gmail.com>
2024-06-07 21:17:45 +0200

    added keywords

--- DeepHouse-Space/vite.config.js
+++ DeepHouse-Space/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   esbuild: {
@@ -9,7 +8,10 @@ export default defineConfig({
     jsxFactory: 'h',
     jsxFragment: 'Fragment'
   },
-  server:{
+  server: {
     port: 3000
+  },
+  build: {
+    outDir: 'dist' 
   }
 });
